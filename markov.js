8905b2a9edca19044f2c2d70767ac91a35407076
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-09-12 15:57:22 -0700

    completed getText() method

--- markov.js
+++ markov.js
@@ -26,25 +26,32 @@ class MarkovMachine {
    *
    * */
 
-  getChains(self) {
+  getChains() {
     // TODO: implement this!
     let chain = {};
 
     for (let i = 0; i < this.words.length; i++) {
+
       if (this.words[i] in chain) {
+
         if (this.words[i+1] === undefined) {
+
           chain[this.words[i]].push(null);
         } else {
+
           chain[this.words[i]].push(this.words[i+1]);
         }
+
       } else {
+
         chain[this.words[i]] = [this.words[i+1]];
+
         if (this.words[i+1] === undefined) {
           chain[this.words[i]] = [null];
         }
       }
-
     }
+
     return chain;
   }
 
@@ -52,26 +59,58 @@ class MarkovMachine {
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
 
-  // getText() {
-  //   // TODO: implement this!
-  //   let randomWords = [];
-  //   let idx = Math.floor(
-  //     Math.random() * this.chains.length
-  //   );
+  getText() {
+  /*
+  psuedo:
+  create the variable to house our sentence.
 
-  //   for (let i = 0; i < this.chains.length; i++) {
-  //     randomWords.push(this.chains[]);
+  starting will be the first key inside of the chains object => push inside var
+  take random index from its array and push it inside
+    if that is null, break and return the sentence
+    if not
+      find the word inside of the object
+        if it is inside, loop inside of its array
+          repeat steps
+  */
 
-  //     if (this === null) {
+    const sentence = [];
+    const wordsInObject = Object.keys(this.chains);
 
-  //     }
-  //   }
-  //   // - start at the first word in the input text
-  //   // - find a random word from the following-words of that
-  //   // - repeat until reaching the terminal null
-  // }
+    let curr_word = wordsInObject[0];
+    sentence.push(curr_word);
+
+    while (curr_word !== null) {
+      let idx = Math.floor(
+        Math.random() * this.chains[curr_word].length
+      );
+
+      curr_word = this.chains[curr_word][idx];
+      sentence.push(curr_word);
+    }
+
+    return sentence.join(' ');
+  }
+
 }
 
 module.exports = {
   MarkovMachine,
 };
+
+// let randomWords = [];
+
+
+//     for (let word in this.chains) {
+//       let idx = Math.floor(
+//         Math.random() * this.chains[word].length
+//       );
+
+//       if (this.chains[word][idx] === null) {
+//         return randomWords.join(' ');
+//       } else {
+
+//         randomWords.push(this.chains[word][idx])
+//       }
+//     }
+
+//     return randomWords.join(' ');
